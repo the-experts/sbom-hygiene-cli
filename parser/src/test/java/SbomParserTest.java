20b6c163787d636fdaf86bfff3cb689540db1504
Sebastiaan <sebastiaan.j.koot@gmail.com>
2025-11-04 15:43:43 +0100

    eerste opzet van de sbom parser java

--- parser/src/test/java/SbomParserTest.java
+++ parser/src/test/java/SbomParserTest.java
@@ -1,5 +1,21 @@
+import com.siemens.sbom.standardbom.StandardBomParser;
+import org.cyclonedx.exception.ParseException;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SbomParserTest {
+
+    private static final File sbom = new File("");
+
+    @Test;
+    void when_sbom_then_return_standardbom() throws IOException, ParseException {
+        var standardBom = new StandardBomParser().parse(sbom);
+        assertThat(standardBom.equals());
+    }
 
 }
